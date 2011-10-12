06a9539c0c3e7475597afe7fad7736e7d282c5a9
Craig Walls <cwalls@vmware.com>
2011-10-12 11:34:30 -0500

    Reorganized the code and took initial steps toward Java-based config

--- oauth/src/main/java/org/springframework/cloud/oauth/Bootstrap.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.cloud.oauth;
-
-public class Bootstrap {
-	public void go() {
-		String vcapServices = System.getenv("VCAP_SERVICES");
-		System.out.println("VCAP SERVICES:  " + vcapServices);		
-	}
-}
