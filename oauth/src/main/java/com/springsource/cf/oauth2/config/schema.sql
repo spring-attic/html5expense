7c092ae312d84159145fdf5bf1b7312239e01dea
Craig Walls <cwalls@vmware.com>
2011-10-13 15:00:29 -0500

    Partial commit of work after rethinking oauth-enablement process

--- oauth/src/main/java/com/springsource/cf/oauth2/config/schema.sql
+++ oauth/src/main/java/com/springsource/cf/oauth2/config/schema.sql
@@ -1,5 +1,16 @@
+drop table oauth_client_details;
 drop table oauth_access_token; 
 drop table oauth_refresh_token; 
+
+create table oauth_client_details (
+  client_id VARCHAR(256) PRIMARY KEY,
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256)
+);
 
 create table oauth_access_token (
   token_id VARCHAR(256),
