72a1d89b5111b8ddbad687e1bcaa6bdd88b236ad
Craig Walls <cwalls@vmware.com>
2011-10-17 14:07:55 -0500

    Added S2OAuth to the API app to secure the reports resources

---
+++ api/src/main/java/com/springsource/html5expense/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.springsource.html5expense.config;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.oauth2.provider.token.JdbcOAuth2ProviderTokenServices;
+
+@Configuration
+@ImportResource({"classpath:com/springsource/html5expense/config/security.xml", "classpath:com/springsource/html5expense/config/oauth.xml"})
+public class SecurityConfig {
+
+	@Inject
+	@Named("tokenDataSource")
+	private DataSource dataSource;
+	
+	@Bean 
+	public JdbcOAuth2ProviderTokenServices tokenServices() {
+		JdbcOAuth2ProviderTokenServices tokenServices = new JdbcOAuth2ProviderTokenServices(dataSource);
+		tokenServices.setSupportRefreshToken(true);
+		return tokenServices;
+	}
+	
+	@Bean
+	public TextEncryptor textEncryptor() {
+		return Encryptors.noOpText();
+	}
+	
+}
