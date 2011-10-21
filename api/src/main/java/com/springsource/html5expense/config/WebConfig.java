9667162e07edf3c2e85d396347775a70e0296980
Roy Clarkson <rclarkson@vmware.com>
2011-10-21 10:44:50 -0400

    Now zipping the contents of the client-assets directory and serving from resources/client/assets.zip

--- api/src/main/java/com/springsource/html5expense/config/WebConfig.java
+++ api/src/main/java/com/springsource/html5expense/config/WebConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -34,6 +35,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("index");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -41,7 +47,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
-    
+
     @Bean
     public MultipartResolver multipartResolver() {
         return new CommonsMultipartResolver();
