b7794de9c2369f7e0404da6a0c3a86da52789852
Roy Clarkson <rclarkson@vmware.com>
2011-10-25 19:35:16 -0500

    Prevent screen orientation from changing to landscape

--- client/android/src/org/springsource/html5expense/Html5expense.java
+++ client/android/src/org/springsource/html5expense/Html5expense.java
@@ -1,5 +1,7 @@
 package org.springsource.html5expense;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.phonegap.DroidGap;
@@ -10,5 +12,11 @@ public class Html5expense extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
