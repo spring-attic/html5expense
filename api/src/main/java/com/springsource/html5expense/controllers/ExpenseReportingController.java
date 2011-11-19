9380a65d38d2738b944c358ac4d894ae8f2feece
Josh Long <josh.long@springsource.com>
2011-11-19 15:48:31 -0500

    adding code..

---
+++ api/src/main/java/com/springsource/html5expense/controllers/ExpenseReportingController.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.springsource.html5expense.controllers;
+
+import com.springsource.html5expense.EligibleCharge;
+import com.springsource.html5expense.Expense;
+import com.springsource.html5expense.ExpenseReport;
+import com.springsource.html5expense.ExpenseReportingService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Roy Clarkson
+ */
+@Controller
+@RequestMapping("/reports")
+public class ExpenseReportingController {
+
+    @Inject
+    private ExpenseReportingService service;
+
+    /**
+     * Create a new {@link com.springsource.html5expense.ExpenseReport} with an associated description for the purpose
+     *
+     * @param purpose the reason for the expense report. i.e. conference, business meal, etc.
+     * @return the ID of the new expense report
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public Long createReport(@RequestParam(required = true) String purpose) {
+        return service.createReport(purpose);
+    }
+
+    /**
+     * Retrieve a list of charges that can be associated with an {@link com.springsource.html5expense.ExpenseReport}.
+     * These charges are not currently associated with any other expense report.
+     *
+     * @return collection of {@link com.springsource.html5expense.EligibleCharge} objects
+     */
+    @RequestMapping(value = "/eligible-charges", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Collection<EligibleCharge> getEligibleCharges() {
+        return service.getEligibleCharges();
+    }
+
+    /**
+     * Associate expenses with an {@link com.springsource.html5expense.ExpenseReport}
+     *
+     * @param reportId  the ID of the {@link com.springsource.html5expense.ExpenseReport}
+     * @param chargeIds the IDs of the {@link EligibleCharge} objects to associate with the expense report
+     * @return
+     */
+    @RequestMapping(value = "/{reportId}/expenses", method = RequestMethod.POST, produces = "application/json")
+
+    @ResponseBody
+    public Collection<Expense> createExpenses(
+            @PathVariable Long reportId,
+            @RequestParam(required = true) List<Long> chargeIds) {
+        return service.createExpenses(reportId, chargeIds);
+    }
+
+    /**
+     * Associate an image of a receipt with an {@link Expense}
+     *
+     * @param reportId     the ID of the {@link com.springsource.html5expense.ExpenseReport}
+     * @param expenseId    the ID of the {@link Expense}
+     * @param receiptBytes the image of the receipt
+     * @return the URI of the image
+     */
+    @RequestMapping(value = "/{reportId}/expenses/{expenseId}/receipt",
+            method = RequestMethod.POST,
+            consumes = "multipart/form-data")
+    @ResponseBody
+    public String attachReceipt(@PathVariable Long reportId, @PathVariable Integer expenseId, @RequestParam(required = true) byte[] receiptBytes) {
+        return service.attachReceipt(reportId, expenseId, receiptBytes);
+    }
+
+    /**
+     * Finalizes and submits the {@link com.springsource.html5expense.ExpenseReport} for review
+     *
+     * @param reportId the ID of the {@link com.springsource.html5expense.ExpenseReport}
+     */
+    @RequestMapping(value = "/{reportId}", method = RequestMethod.POST)
+    public void submitReport(@PathVariable Long reportId) {
+        service.submitReport(reportId);
+    }
+
+    /**
+     * Retrieves all of the open, or incomplete, expense reports for the user
+     *
+     * @return list of {@link com.springsource.html5expense.ExpenseReport} objects
+     */
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<ExpenseReport> getOpenReports() {
+        return service.getOpenReports();
+    }
+
+}
---
+++ api/src/test/java/com/springsource/html5expense/impl/TestJpaExpenseReportingService.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.springsource.html5expense.impl;
+
+import com.springsource.html5expense.*;
+import com.springsource.html5expense.config.ComponentConfig;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+import java.util.*;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = ComponentConfig.class)
+public class TestJpaExpenseReportingService {
+    private String itsMission = "To boldly go where no application's gone before!";
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Inject
+    private ExpenseReportingService expenseReportingService;
+
+    @Inject
+    private PlatformTransactionManager transactionManager;
+
+    private EligibleCharge expensiveCharge;
+    private List<EligibleCharge> charges;
+
+
+    @Before
+    public void installSomeCharges() throws Throwable {
+        final List<EligibleCharge> chargesToAdd = Arrays.asList(
+                new EligibleCharge(new Date(), "Starbucks", "food", new BigDecimal(4)),
+                new EligibleCharge(new Date(), "dinner at Morton's Steak House", "food", new BigDecimal(59.99)));
+
+        // clean out the data
+        TransactionTemplate transactionTemplate = new TransactionTemplate(this.transactionManager);
+        transactionTemplate.execute(new TransactionCallback<Object>() {
+            public Object doInTransaction(TransactionStatus status) {
+
+                entityManager.createQuery("DELETE FROM " + Expense.class.getName()).executeUpdate();
+                entityManager.createQuery("DELETE FROM " + ExpenseReport.class.getName()).executeUpdate();
+                entityManager.createQuery("DELETE FROM " + EligibleCharge.class.getName()).executeUpdate();
+                return null;
+            }
+        });
+
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                for (EligibleCharge ec : chargesToAdd) {
+                    entityManager.persist(ec);
+                }
+            }
+        });
+
+        charges = new ArrayList<EligibleCharge>(expenseReportingService.getEligibleCharges());
+        expensiveCharge = charges.get(1);
+    }
+
+
+    @Test
+    public void testCreateReport() throws Throwable {
+        Long expenseReportId = expenseReportingService.createReport(itsMission);
+        Assert.assertTrue("the ID must be greater than 0", expenseReportId > 0);
+        Assert.assertEquals(1, expenseReportingService.getOpenReports().size());
+        Assert.assertEquals(entityManager.find(ExpenseReport.class, expenseReportId).getPurpose(), itsMission);
+    }
+
+    @Test
+    public void testIdentifyingEligibleCharges() throws Throwable {
+        Collection<EligibleCharge> eligibleCharges = expenseReportingService.getEligibleCharges();
+        Assert.assertTrue(eligibleCharges.size() == this.charges.size());
+    }
+
+    @Test public void testCreateExpenses() throws Throwable {
+    Long expenseReportId = expenseReportingService.createReport(itsMission);
+    List<Long> chargeIds = new ArrayList<Long>();
+    for (EligibleCharge ec : charges) {
+    chargeIds.add(ec.getId());
+    }
+    Collection<Expense> expenseCollection = expenseReportingService.createExpenses(expenseReportId, chargeIds);
+    Assert.assertNotNull(expenseCollection);
+    Assert.assertTrue(expenseCollection.size() == 2);
+    }
+
+    @Test public void testAttachingReceipts() throws Throwable {
+    Long expenseReportId = expenseReportingService.createReport(itsMission);
+    Collection<Expense> expenses = expenseReportingService.createExpenses(expenseReportId, Arrays.asList(expensiveCharge.getId()));
+    Integer expenseId = expenses.iterator().next().getId();
+    String receiptClaim = expenseReportingService.attachReceipt(expenseReportId, expenseId, new byte[0]);
+    Assert.assertNotNull(receiptClaim);
+    Expense entity = entityManager.find(Expense.class, expenseId);
+    Assert.assertFalse(entity.isFlagged());
+    Assert.assertEquals(receiptClaim, entity.getReceipt());
+    }
+
+    @Test
+    @Transactional
+    public void testSubmittingReports() throws Throwable {
+    Long expenseReportId = expenseReportingService.createReport(itsMission);
+    expenseReportingService.submitReport(expenseReportId);
+    ExpenseReport er = entityManager.find(ExpenseReport.class, expenseReportId);
+    Assert.assertEquals(er.getState(), State.IN_REVIEW);
+    }
+
+}
+/**
+
+ @RunWith(SpringJUnit4ClassRunner.class)
+ @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = ComponentConfig.class)
+ public class TestJpaExpenseReportingService {
+
+ private EligibleCharge expensiveCharge;
+
+ @Inject private ExpenseReportingService expenseReportingService;
+
+ @Inject private PlatformTransactionManager transactionManager;
+
+ @PersistenceContext private EntityManager entityManager;
+
+ @Inject private DataSource dataSource;
+
+ private String itsMission = "\"to go... where no man... has gone before!\"";
+
+ private List<EligibleCharge> charges;
+
+ }
+ */
